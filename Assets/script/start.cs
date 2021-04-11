be143d7c5f7cb430761a46f1eb6fbfc86aeacc17
ysk <kongjia8211@icloud.com>
2021-04-11 23:16:50 +0900

    １

---
+++ Assets/script/start.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("select");
+    }
+
+}
